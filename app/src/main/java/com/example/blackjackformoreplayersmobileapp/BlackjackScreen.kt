2610d5f2d73cb84801c3fba41913facef4fe4218
Konstantyn <gagarin2789@gmail.com>
2023-07-26 13:28:34 +0200

    Created screen files

--- app/src/main/java/com/example/blackjackformoreplayersmobileapp/BlackjackScreen.kt
+++ app/src/main/java/com/example/blackjackformoreplayersmobileapp/BlackjackScreen.kt
@@ -1,6 +1,19 @@
 package com.example.blackjackformoreplayersmobileapp
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.blackjackformoreplayersmobileapp.ui.BlackjackViewModel
+
 enum class BlackjackScreen {
     MainMenu, Rules, InputAmountOfPlayers, InputNameOfPlayers, Game, Result
+}
+
+@Composable
+fun BlackjackApp(
+    viewModel: BlackjackViewModel = viewModel(),
+) {
+    val uiState by viewModel.uiState.collectAsState()
 }
 
